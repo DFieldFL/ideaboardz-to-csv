f996e21b1b24bba80651ee682360e9ff15837f6b
Brandon Groves <brandon.groves@gmail.com>
2017-07-12 22:59:40 -0400

    Initial checkin of code

---
+++ exportToCsv.py
@@ -0,0 +1,30 @@
+import csv
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+
+url = raw_input("Enter a ideazboard url: ")
+browser = webdriver.Firefox(executable_path = '/Users/DFieldFL/bin/geckodriver')
+browser.get(url)
+time.sleep(4)
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+with open('out.csv', 'wb') as csvfile:
+    csvWriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    columns = []
+    for section in soup.find_all('div', class_='section'):
+        sectionText = ''
+        for sticky in section.find_all('div', class_='sticky'):
+            stickyText = sticky.find('div', class_='stickyText').getText()
+            stickyVotes = sticky.find('span', class_='count').getText()
+            votes = ' (+' + stickyVotes + ')' if stickyVotes != '0' else ''
+            sectionText += stickyText + votes + '\r\n'
+        columns.append(sectionText)
+
+    csvWriter.writerow(columns)
+
+browser.quit()
